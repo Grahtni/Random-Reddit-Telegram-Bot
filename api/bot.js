fba432c3211459b4d4f2387b72c41593fec4a202
Zubin <me@zubin.org.in>
2023-02-27 16:10:50 +0530

    Integrate Gfycat support

--- api/bot.js
+++ api/bot.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Bot, webhookCallback, HttpError, GrammyError } = require("grammy");
 const RandomReddit = require("reddit-posts");
 const path = require("path");
+const { gfycat } = require("gfycat-api");
 
 // Bot
 
@@ -77,7 +78,17 @@ bot.command("help", async (ctx) => {
 // Messages
 
 bot.on("msg", async (ctx) => {
-  console.log("Query received:", ctx.msg.text, "from", ctx.from.id);
+  // Logging
+
+  const from = ctx.from;
+  const name =
+    from.last_name === undefined
+      ? from.first_name
+      : `${from.first_name} ${from.last_name}`;
+  console.log(
+    `From: ${name} (@${from.username}) ID: ${from.id}\nMessage: ${ctx.msg.text}`
+  );
+  // Logic
 
   if (!/^[a-zA-Z0-9_-]+$/.test(ctx.msg.text)) {
     await ctx.reply("*Send a valid subreddit name like cats or aww.*", {
@@ -85,14 +96,26 @@ bot.on("msg", async (ctx) => {
       reply_to_message_id: ctx.msg.message_id,
     });
   } else {
-    await ctx
-      .reply(`*Getting posts from r/${ctx.msg.text}*`, {
+    const statusMessage = await ctx.reply(
+      `*Getting posts from r/${ctx.msg.text}*`,
+      {
         parse_mode: "Markdown",
-      })
-      .catch((error) => console.error(error));
+      }
+    );
+    async function deleteMessageWithDelay(fromId, messageId, delayMs) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          bot.api
+            .deleteMessage(fromId, messageId)
+            .then(() => resolve())
+            .catch((error) => reject(error));
+        }, delayMs);
+      });
+    }
+    await deleteMessageWithDelay(ctx.from.id, statusMessage.message_id, 3000);
     try {
-      for (let i = 0; i < 5; i++) {
-        if (i == 4) {
+      for (let i = 0; i < 6; i++) {
+        if (i == 5) {
           break;
         }
         const data = await RandomReddit.GetRandompost(ctx.msg.text);
@@ -102,14 +125,14 @@ bot.on("msg", async (ctx) => {
         const author = data.Author.replace(markdownChars, "\\$&");
 
         if (
-          extension === ".jpg" ||
-          (extension === ".png" && !data.ImageURL.match(".gif.jpg"))
+          (extension === ".jpg" && !data.ImageURL.match(".gif.jpg")) ||
+          extension === ".png"
         ) {
           await ctx.replyWithPhoto(data.ImageURL, {
             caption: `[${title}](${data.url})\n${data.UpVotes} upvotes\nBy ${author}`,
             parse_mode: "Markdown",
           });
-        } else if (data.ImageURL.match(".gif.jpg")) {
+        } else if (data.ImageURL.includes(".gif.jpg")) {
           let newUrl = data.imageUrl.slice(0, data.imageUrl.lastIndexOf("."));
           await ctx.replyWithVideo(newUrl, {
             caption: `[${title}](${data.url})\n${data.UpVotes} upvotes\nBy ${author}`,
@@ -152,12 +175,26 @@ bot.on("msg", async (ctx) => {
       }
     } catch (error) {
       if (error instanceof GrammyError) {
+        if (error.message.includes("Forbidden: bot was blocked by the user")) {
+          console.log("Bot was blocked by the user");
+        } else if (error.message.includes("Call to 'sendVideo' failed!")) {
+          console.log("Error sending video");
+          await ctx.reply(`*Error contacting Reddit.*`, {
+            parse_mode: "Markdown",
+            reply_to_message_id: ctx.msg.message_id,
+          });
+        } else {
+          await ctx.reply(`*An error occurred: ${error.message}*`, {
+            parse_mode: "Markdown",
+            reply_to_message_id: ctx.msg.message_id,
+          });
+        }
         console.log(`Error sending message: ${error.message}`);
         return;
       } else {
-        console.log("An error occurred");
+        console.log(`An error occured:\n${error}`);
         await ctx.reply(
-          `*An error occurred. Are you sure you sent a valid subreddit name?*`,
+          `*An error occurred. Are you sure you sent a valid Reddit link?*\n_Error: ${error.message}_`,
           { parse_mode: "Markdown", reply_to_message_id: ctx.msg.message_id }
         );
         return;
